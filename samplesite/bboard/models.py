934de60183199d935a87e04a12c687ce513fe240
canto9112 <cantoweb112@gmail.com>
2021-09-10 18:01:09 +0300

    Добавил класс Meta в модель Bb

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -7,3 +7,7 @@ class Bb(models.Model):
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
 
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-published']
