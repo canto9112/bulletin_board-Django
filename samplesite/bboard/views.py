9d710a507afc0179fdf900b9657d3fd2e161af7c
canto9112 <cantoweb112@gmail.com>
2021-09-10 17:36:47 +0300

    Сократил код функции index

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bb
 
 
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений.')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+
